e34822e333ee2a482b6318872737dc92e9bc2773
Raymond Mei <raymondmei.rm@gmail.com>
2024-11-15 23:39:05 -0500

    start backtest module

--- fetchfinancialdata/urls.py
+++ fetchfinancialdata/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, getstockdata
+from .views import index, getstockdata, backtest
 
 urlpatterns = [
 	path('', view=index, name='index'),
 	path('getstockdata', view=getstockdata, name='getstockdata'),
+	path('backtest', view=backtest, name='backtest'),
 ]
