40f799ca902232135e05cdfd9eeae2d823ffcd71
Raymond Mei <raymondmei.rm@gmail.com>
2025-01-19 02:17:45 -0500

    dockerize front and back end

--- backend/FinancialDataManagementSystem/settings.py
+++ backend/FinancialDataManagementSystem/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ 
+DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
+ 
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTs","127.0.0.1").split(",")
 
 # Application definition
 
